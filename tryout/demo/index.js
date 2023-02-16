9166faa198ffea9594791e2e805a5ec8a5ce25ae
Desislava <desi.peeva@gmail.com>
2023-02-16 15:45:21 +0200

    abstract out common layout and navigation

--- tryout/demo/index.js
+++ tryout/demo/index.js
@@ -1,24 +1,12 @@
 const http = require("http")
 
 const homePage = `
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    <a href="/about">About</a>
     <h1>Home Page</h1>
     <p>Welcome to our site</p>
-</body>
-</html>
 `
 const aboutPage = `
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    <a href="/">Home</a>
     <h1>About Us</h1>
     <p>Contact: +1-555-1973</p>
-</body>
-</html>
 `
 
 const server = http.createServer((request, response) => {
@@ -28,15 +16,32 @@ const server = http.createServer((request, response) => {
     console.log(url)
 
     if (url.pathname == "/") {
-        response.write(homePage)
+        response.write(html(homePage))
         response.end()
     } else if (url.pathname == "/about") {
-        response.write(aboutPage)
+        response.write(html(aboutPage))
         response.end()
     } else {
         response.statusCode = 404
+        response.write("Not found")
         response.end()
     }
 })
 
 server.listen(3000)
+
+function html(body) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <body>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/catalog">Catalog</a>
+            <a href="/about">About</a>
+        </nav>
+        ${body}
+    </body>
+    </html>
+    `
+}
