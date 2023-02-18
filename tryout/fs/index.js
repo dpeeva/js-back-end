ea99e49bd3b0479eb65b0deb17378dfd54be8b19
Desislava <desi.peeva@gmail.com>
2023-02-18 16:12:17 +0200

    resolve file not found issue

--- tryout/fs/index.js
+++ tryout/fs/index.js
@@ -1,5 +1,8 @@
 const fs = require("fs")
+const path = require("path")
 
-const text = fs.readFileSync("./demo.txt")
+const text = fs.readFileSync(
+    path.resolve(__dirname, "demo.txt")
+)
 
 console.log(text.toString())
