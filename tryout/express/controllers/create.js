2be65e5c2ea539484bcae1a09b3edefd431326b1
Desislava <desi.peeva@gmail.com>
2023-02-18 20:55:55 +0200

    test adding middleware to all pages

--- tryout/express/controllers/create.js
+++ tryout/express/controllers/create.js
@@ -8,15 +8,21 @@ router.get("/", (req, res) => {
     )
 })
 
+// const middleware = (req, res, next) => {
+//     console.log("Handling POST request")
+//     console.log("Logging from middleware")
+//     next()
+// }
+
 router.post("/",
-    (req, res, next) => {
-        console.log("Handling POST request")
-        console.log("Logging from middleware")
-        next()
-    },
+    // (req, res, next) => middleware(req, res, next),
     (req, res) => {
         res.redirect("/catalog")
     }
 )
 
 module.exports = router
+// module.exports = {
+//     create: router,
+//     middleware
+// }
