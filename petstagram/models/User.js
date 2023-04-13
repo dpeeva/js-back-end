7db6e7da03dbb32e5f9dd65e750e8d97fd1c7399
Desislava <desi.peeva@gmail.com>
2023-04-13 13:51:41 +0300

    update User model

--- petstagram/models/User.js
+++ petstagram/models/User.js
@@ -2,11 +2,15 @@ const { Schema, model } = require("mongoose")
 
 const userSchema = new Schema({
     username: {
-        type: String, required: true, unique: true,
+        type: String,
+        required: true,
+        unique: true,
         minlength: [2, "Username must be at least 2 characters long"]
     },
     email: {
-        type: String, required: true, unique: true,
+        type: String,
+        required: true,
+        unique: true,
         minlength: [10, "Email must be at least 10 characters long"]
     },
     hashedPassword: { type: String, required: true }, // service generates the password
@@ -15,15 +19,6 @@ const userSchema = new Schema({
 // needed for uniqueness of username
 userSchema.index({
     username: 1, // use -1 for descending order
-}, {
-    collation: {
-        locale: "en",
-        strength: 2 // for case-insensitive
-    }
-})
-
-userSchema.index({
-    email: 1, // use -1 for descending order
 }, {
     collation: {
         locale: "en",
