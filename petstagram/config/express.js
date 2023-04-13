b1b3111c1badde7a433a363947e39eb24ad08db1
Desislava <desi.peeva@gmail.com>
2023-04-13 13:01:11 +0300

    fix typos

--- petstagram/config/express.js
+++ petstagram/config/express.js
@@ -1,22 +1,17 @@
 const express = require("express")
 const handlebars = require("express-handlebars")
-const path = require("path")
 const cookieParser = require("cookie-parser")
 const session = require("../middlewares/session")
 const trimBody = require("../middlewares/trimBody")
 
 module.exports = (app) => {
     const hbs = handlebars.create({
-        extname: "hbs",
+        extname: ".hbs",
     })
-    app.set("views", path.join(__dirname, "../views"))
     app.engine(".hbs", hbs.engine)
     app.set("view engine", ".hbs")
+    app.use("/static", express.static("static"))
 
-    app.use(
-        path.resolve(__dirname, "/static"),
-        express.static("static")
-    )
     app.use(express.urlencoded({ extended: true }))
     app.use(cookieParser())
     app.use(session())
